6c643bb84a6aadbf7a4cb54e8ef9c303408d0d09
Alvind0 <170922866+Alvind0@users.noreply.github.com>
2025-10-24 08:55:06 +0900

    Change factorio-end.bat to only execute git push if git commit succeeds, meaning if there is nothing changed, nothing will get pushed

--- factorio-end.bat
+++ factorio-end.bat
@@ -4,5 +4,4 @@ cd %APPDATA%/Factorio
 for /f "delims=" %%a in ('powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-Date -Format 'yyyy/MM/dd HH:mm'"') do set "DATETIME=%%a"
 
 git add .
-git commit -m "%DATETIME% sync"
-git push
+git commit -m "%DATETIME% sync" && git push
