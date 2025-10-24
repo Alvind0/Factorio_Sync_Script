5d4b7948790a85ac1684458a9c17b182f18ce079
Alvind0 <170922866+Alvind0@users.noreply.github.com>
2025-10-24 10:03:23 +0900

    Combine start and end scripts
    Merges the logic from factorio_start.bat and factorio_end.bat into a single launch_factorio.bat.
    The new script now handles the entire workflow: git pull, launching the game, and then committing/pushing upon exit.

--- factorio-start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd %APPDATA%/Factorio
-git pull
--- launch_factorio.bat
+++ launch_factorio.bat
@@ -1,6 +1,10 @@
 @echo off
 cd %APPDATA%/Factorio
+git pull
 
+start "" /wait "c:\Program Files\Factorio\bin\x64\factorio.exe"
+
+cd %APPDATA%/Factorio
 for /f "delims=" %%a in ('powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-Date -Format 'yyyy/MM/dd HH:mm'"') do set "DATETIME=%%a"
 
 git add .
